a764b27ea1c26f2707de915d233353ad0d9cafd9
Good0809 <98064706+Good0809@users.noreply.github.com>
2022-02-18 08:01:47 -0600

    Added some mroe comments
    Added some more comments. Code is beautiful. This marks the 13th hour Ive worked on this.

--- project2butimnotdumb.py
+++ project2butimnotdumb.py
@@ -14,7 +14,8 @@ puttheta(s,k,r,si,t)
 For all functions the variables are in the same. exact. order.
 errors will be returned in any of them are not floats/ints and if si or t is 0.
 '''
-
+#importing all of the greeks since it did not state we needed to use their binomial tree approximations. All the code should be the same as from last time, though
+#all r have been modified to (r-q) since we need to factor in the continuous dividend rate
 #creating the "anonymous functions" for d1 and d2 which will help us in our later endeavors.
 d1 = lambda s,q,k,r,si,t: (np.log(s/k) + ((r-q) + ((si**2)/2)*t))/(si*np.sqrt(t))
 d2 = lambda s,q,k,r,si,t: d1(s,q,k,r,si,t) - si*np.sqrt(t) #d2 can call d1 for a simpler calculation.
@@ -52,8 +53,8 @@ def testgreek(s,q,k,r,si,t):
     print('call rho is', callrho(s,q,k,r,si,t))
     print('put rho is', putrho(s,q,k,r,si,t))
 
-def stock_sim(s0,s,u,n,l = 0, m=0,):
-    if m < l+1 and l<n+1:
+def stock_sim(s0,s,u,n,l = 0, m=0,): #here we simulate the branches of the tree for stock using this poor mans for loop
+    if m < l+1 and l<n+1: #iterates much like the code did last year. generates every "possible" outcome.
         s[m,l] = s0*u**(2*m - l)
         m += 1
         return  stock_sim(s0,s,u,n,l,m)
@@ -63,7 +64,7 @@ def stock_sim(s0,s,u,n,l = 0, m=0,):
         return stock_sim(s0,s,u,n,l,m)
     else:
         return s
-def step1(s0,k,r,sigma,T,n): #this will get our 
+def step1(s0,k,r,sigma,T,n): #this will get our stockprices
     s = np.zeros([n+1,n+1])
     j = n+1
     dt = T/n
@@ -73,9 +74,9 @@ def step1(s0,k,r,sigma,T,n): #this will get our
 
     return stockprices
 def pricefind(S,k,Acall,Call,Ecall, Aput,Put,Eput, p, r, q,dt,n,contput, contcall, conteput, contecall, l = 1, m = 0):
-    x = np.zeros([n+1,n+1])
+    x = np.zeros([n+1,n+1]) #we need this for the europeans of the tree
     if (l < n+1) and (m < n+1-l):
-        disc = np.exp(-(r-q)*dt)
+        disc = np.exp(-(r-q)*dt) #our discount factor, easier to do it this way than retyping it
         contcall[m] = (Acall[m,-l]*(1-p) + Acall[m+1,-l]*p)* disc
         contecall[m] = (Ecall[m,-l]*(1-p) + Ecall[m+1,-l]*p)* disc
         contput[m] = (Aput[m,-l]*(1-p) + Aput[m+1,-l]*p)* disc
@@ -83,7 +84,7 @@ def pricefind(S,k,Acall,Call,Ecall, Aput,Put,Eput, p, r, q,dt,n,contput, contcal
         m+=1
         pricefind(S,k,Acall,Call,Ecall,Aput,Put,Eput,p,r,q,dt,n,contput,contcall,conteput,contecall,l=l,m=m)
     if l < n+1:
-        Acall[:,-(l+1)] = np.maximum(Call[:,-(l+1)],contcall)
+        Acall[:,-(l+1)] = np.maximum(Call[:,-(l+1)],contcall) #check for the maxes using the aforementioned arrays
         Ecall[:,-(l+1)] = np.maximum(x[:,-(l+1)], contecall)
         Aput[:,-(l+1)] = np.maximum(Put[:,-(l+1)],contput)
         Eput[:,-(l+1)] = np.maximum(x[:,-(l+1)], conteput)
@@ -115,16 +116,20 @@ def finished_product(s0,k,r,q,sigma,T,n):
     
     finallist = pricefind(stockprices, k, Acall,Call,Ecall,Aput,Put,Eput,p,r,q,dt,n,contput = np.zeros(n+1), contcall = np.zeros(n+1),conteput = np.zeros(n+1), contecall = np.zeros(n+1), l = 1, m = 0)
     
+    #super long formula but unfortunately to create another poormans forloop, I had to...
+    #really this whole thing has made me realize how much Ive taken for loops for granted
+    #might make a shrine to them in the middle of my apartment lobby who knows
+    
     amcall = finallist[0][0,0]
-    eucall = finallist[1][0,0]
-    amput = finallist[2][0,0]
+    eucall = finallist[2][0,0]
+    amput = finallist[1][0,0]
     euput = finallist[3][0,0]
     
-    print('The American Call is ', amcall)
+    print('The American Call is ', amcall) #prints all of our options prices
     print('The European Call is ', eucall)
     print('The American Put is ', amput)
     print('The European Put is ', euput)
     
-    testgreek(s0,q,k,r,sigma,T)
+    testgreek(s0,q,k,r,sigma,T) #prints our greeks
     return
-finished_product(50,50,0.02,0.04,0.3,1,6)
+finished_product(50,50,0.02,0.04,0.3,1,6) #here's the test as requested last time, hope this is enough haha
